23646a844fc1bf4640161901ee7ec90e740b9b8c
Desiree Mendes <desireefrost@live.ca>
2022-11-21 10:33:50 -0500

    background colour and font colours

--- business-card/src/components/Header.js
+++ business-card/src/components/Header.js
@@ -1,11 +1,15 @@
+import './Header.css'
+
 function Header () {
     return (
       <div className="Header">
         
           {/* <img src={logo} className="header-photo" /> */}
           <h1>Desiree Mendes</h1>
-          <p>
+          <p id="about-me">
             Frontend Developer
+          </p>
+          <p>
             https://personal-portfolio-one.vercel.app/
           </p>
           <button>Email</button>
