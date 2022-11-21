7c3224ff4edf5f0c5b1b550af25f4c093cafff07
Desiree Mendes <desireefrost@live.ca>
2022-11-20 23:52:32 -0500

    add static paragraphs and sections for the card

---
+++ business-card/src/components/About.js
@@ -0,0 +1,18 @@
+import Header from "./Header";
+
+function About () {
+    return (
+      <div className="About">
+          <h1>About</h1>
+          <p>
+            About me section
+          </p>  
+          <h1>Interests</h1>
+          <p>
+            Interests section
+          </p>
+      </div>
+    );
+  }
+
+  export default About;
---
+++ business-card/src/components/Header.js
@@ -0,0 +1,18 @@
+function Header () {
+    return (
+      <div className="Header">
+        
+          {/* <img src={logo} className="header-photo" /> */}
+          <h1>Desiree Mendes</h1>
+          <p>
+            Frontend Developer
+            https://personal-portfolio-one.vercel.app/
+          </p>
+          <button>Email</button>
+          <button>LinkedIn</button>
+      
+      </div>
+    );
+  }
+
+  export default Header;
